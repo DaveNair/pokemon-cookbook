f80a0d116346252a4fe156662e253c3b4ae375c1
DaveNair <mdavenair@gmail.com>
2025-04-24 15:55:47 -0400

    adding basic logic for extracting pokedata

---
+++ lib/core.py
@@ -0,0 +1,30 @@
+## Core file for pkmn-cookbook
+## by Dave Nair
+import json
+from pathlib import Path
+import sys
+
+# --- Step 1: Startup logic (pathing, env vars) ---
+## this will eventually go into MAIN
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT))
+
+POKEDEX_PATH = ROOT / Path("data/processed/pokedex.json")
+
+def load_pokedex():
+	with open(POKEDEX_PATH, "r", encoding="utf-8") as f:
+		return json.load(f)
+
+POKEDEX = load_pokedex()
+
+def get_pokedata(pkname, dexkey):
+	try:
+		return POKEDEX.get(
+			pkname.strip().lower(), 
+			{dexkey: None}
+			).get(dexkey, None)
+	except Exception as e:
+		print(f"[{e}] Could not retrieve {pkname.strip().lower()}.{dexkey} from POKEDEX.")
+		sys.exit(1)
+	return None
+
