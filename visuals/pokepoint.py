0258d23774e4fe71e54d6433ff8e8e8de269d7a5
DaveNair <mdavenair@gmail.com>
2025-04-29 23:57:01 -0400

    made some big logic changes - now pokepoint is layering 5 concentric circles: status ring, main 4-quad, status dmg ring, attack dmg ring, remaining-health 4-quad. have not yet tested

--- visuals/pokepoint.py
+++ visuals/pokepoint.py
@@ -1,5 +1,6 @@
 from matplotlib.patches import Wedge, Circle
 import matplotlib.pyplot as plt
+import numpy as np
 from pathlib import Path
 import sys
 
@@ -14,10 +15,8 @@ class Pokepoint:
         self.types = types if len(types)==2 else types*2
         self.size = size
         self.hp_pct = max(0, min(hp_pct, 1.0))  # Clamp to 0–1
-        self.hpp_last = self.hp_pct
-        self.total_burn_dmg = 0
-        self.total_poison_dmg = 0
-        self.total_toxic_dmg = 0
+        self.hpp_last = None
+        self.total_status_dmg = 0
         self.status = status
 
     def set_status(self, status):
@@ -29,18 +28,17 @@ class Pokepoint:
     def add_burn_dmg(self):
         current_burn_dmg = None
         self.decrease_hp(current_burn_dmg)
-        self.total_burn_dmg += current_burn_dmg
+        self.total_status_dmg += current_burn_dmg
 
     def add_poison_dmg(self):
         current_psn_dmg = None
         self.decrease_hp(current_psn_dmg)
-        self.total_poison_dmg += current_psn_dmg
+        self.total_status_dmg += current_psn_dmg
 
     def add_toxic_dmg(self, toxic_turn):
-        ''' FINISH THIS'''
         current_toxic_dmg = toxic_turn / 16.0
         self.decrease_hp(current_toxic_dmg)
-        self.total_toxic_dmg += current_toxic_dmg
+        self.total_status_dmg += current_toxic_dmg
 
     def set_health(self, hp_pct):
         self.hp_pct = max(0, min(hp_pct, 1.0))
@@ -77,27 +75,42 @@ class Pokepoint:
                           facecolor=color, edgecolor='black', linewidth=0.5, zorder=2)
             ax.add_patch(wedge)
 
+        ## using these vars: leftover_r & leftover_hpp? (for all these new dmg layers)
+        ## it is associated with leftover_hpp by the following relationship:    (leftover_r / base_r) == sqrt(leftover_hpp)
+
         # --- ADDING LAYER: Status DAMAGE
+        if self.status and self.total_status_dmg>0:
+            ## figure out the new leftover amt
+            leftover_hpp = 1 - self.total_status_dmg
+            post_status_r = np.sqrt(leftover_hpp) * base_r
+            ## apply "ring" - which is a low-alpha circle, with high-alpha circles inside
+            status_dmg_ring = Circle((x, y), radius=base_r, facecolor=STATUS_COLORS[self.status], alpha=0.6, edgecolor='none', zorder=3)
+            ax.add_patch(status_dmg_ring)
 
         # --- (technically adding a layer in below logic)
 
         # --- Draw delta-hp SHRINKING circle (if not full health) ---
+        if leftover_hpp!=self.hp_pct:
+            damaged_hpp = leftover_hpp - self.hp_pct ## this is the grey portion, not actually used because we already have our final hpp
+            attack_dmg_ring = Circle((x, y), radius=post_status_r, facecolor='#888888', alpha=0.8, edgecolor='none', zorder=4)
+            ax.add_patch(attack_dmg_ring)
+        ## finally, we can draw the health circle (inner circle), using the same 4-quad technique!
+        if self.hp_pct <= 0.25:
+            hp_ring_color = 'red'
+        elif self.hp_pct <= 0.5:
+            hp_ring_color = 'yellow'
+        else:
+            hp_ring_color = 'green'
+        inner_r = np.sqrt(self.hp_pct) * base_r
+        for i, (theta1, theta2) in enumerate(angles):
+            color = TYPE_COLORS.get(self.types[i % 2], "#888888")
+            wedge = Wedge(center=(x, y), r=inner_r, theta1=theta1, theta2=theta2, 
+                facecolor=color, 
+                # edgecolor='black', 
+                edgecolor=hp_ring_color, 
+                linewidth=0.5, zorder=99)
+            ax.add_patch(wedge)
 
-        # --- Draw inner health hole (if not full health) --- <<< THIS IS GETTING REPLACED ABOVE & BELOW
-        if self.hp_pct < 1.0:
-            inner_r = base_r * (1.0 - self.hp_pct)
-            if self.hp_pct <= 0.25:
-                hp_ring_color = 'red'
-            elif self.hp_pct <= 0.5:
-                hp_ring_color = 'yellow'
-            else:
-                hp_ring_color = 'green'
-            inner_circle = Circle((x,y), radius=inner_r, 
-                facecolor='#888888', alpha=0.8, 
-                # edgecolor=hp_ring_color, linewidth=0.25, 
-                zorder=3)
-            ax.add_patch(inner_circle)
 
-            ## after adding the inner "ring", we will need to add an inner circle with normal colors - showing remaining hp
 
 
